4f1da803973c5da473b6107b9eeb3d7aba0e1a21
Christian <rcchristiane@gmail.com>
2013-10-14 21:33:22 -0500

    secuencia leds

--- gpios/urls.py
+++ gpios/urls.py
@@ -9,9 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'gpios.views.home', name='home'),
     # url(r'^gpios/', include('gpios.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^leds/(?P<action>\w{0,50})$', 'leds.views.leds'),
 )
