e96728721e537a60e527d0891d7b325fd8744ffd
Christian <rcchristiane@gmail.com>
2013-10-14 21:52:23 -0500

    leds module

--- leds/views.py
+++ leds/views.py
@@ -2,11 +2,11 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
-from leds.forms import ledsOnOff
+import RPi.GPIO as GPIO
 
 def leds(request,action):
   action = int(action)
-  if ((action != 0) and (action != 1)):
+  if ((action != 0) and (action != 1) or (action )):
     i = { 'Iniciar secuencia de leds?'}
   elif action == 1:
     i = { 'Iniciando secuencia led'}
